390b660f68e803dcf34758e6f12e627f10a5fd39
Miguel Angel Robles Roldan <miguel.robles@atmosfera.unam.mx>
2022-04-26 00:04:37 -0500

    update wtmark.sh
    agregando extracción de nombre base

--- wtmark.sh
+++ wtmark.sh
@@ -1,1 +1,6 @@
 composite -gravity SouthWest -geometry +25+65 ioa_logo_bn_100px.png $1 watermark.jpg
+filename=$(basename -- "$1")
+echo filename $filename
+ext="${filename##*.}"
+base="${filename%.*}"
+echo $base
