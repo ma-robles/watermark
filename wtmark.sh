6f0a62e4c1260d0743f1b8cc0e7652f9ac35e11d
Miguel Robles <miguel.robles@atmosfera.unam.mx>
2022-04-27 12:44:51 -0500

    Se cambió logo por banner (3 logos).

--- wtmark.sh
+++ wtmark.sh
@@ -1,4 +1,4 @@
-composite -gravity SouthWest -geometry +25+65 ioa_logo_bn_100px.png $1 watermark.jpg
+composite -gravity SouthWest -geometry +25+65 banner_300x100.png $1 watermark.jpg
 filename=$(basename -- "$1")
 echo filename $filename
 ext="${filename##*.}"
